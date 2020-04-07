0ee9a6021dcec8844a7bf020be6f127ebf9fea9b
Sharan Kumar <sharanblr@yahoo.co.in>
2020-04-08 02:46:52 +0530

    Updated network url

--- src/utils/network.js
+++ src/utils/network.js
@@ -3,7 +3,7 @@ import { isLinkRegex } from "../constants";
 export const callServerless = async url => {
   if (isLinkRegex.test(url)) {
     try {
-      const res = await fetch(`/api/test?link=${url}`);
+      const res = await fetch(`/api/getPreview?link=${url}`);
       const json = await res.json();
       console.log("RESPONSE FROM FETCH", json);
       return { ...json, responseReceived: true };
