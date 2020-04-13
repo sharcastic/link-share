a7b576e8a8f280ec00595af431edfbba11514cb0
Sharan Kumar <sharanblr@yahoo.co.in>
2020-04-13 17:08:48 +0530

    Changed redirect url in package.json

--- src/components/LinkPreview/index.js
+++ src/components/LinkPreview/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const LinkPreview = ({ preview: { error, icon, title, description } }) => {
+const LinkPreview = ({ preview: { error, image, title, description } }) => {
   return (
     <div>
       {error ? (
         <span>{error}</span>
       ) : (
         <div>
-          <img src={icon} alt="Link Preview" />
+          <img src={image} alt="Link Preview" />
           <span>{title}</span>
           <p>{description}</p>
         </div>
